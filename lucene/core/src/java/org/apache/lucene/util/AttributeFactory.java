954d440e3d8da6f37af235b0c90782a3ab5dcdf1
zhangxuej <957168943@qq.com>
2015-08-17 08:28:26 +0800

    Change-Id: I066c3e5a78a3a01184d3deb757d6445102b41140

--- lucene/core/src/java/org/apache/lucene/util/AttributeFactory.java
+++ lucene/core/src/java/org/apache/lucene/util/AttributeFactory.java
@@ -1,9 +1,5 @@
 package org.apache.lucene.util;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -21,11 +17,15 @@ import java.lang.invoke.MethodType;
  * limitations under the License.
  */
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 /**
  * An AttributeFactory creates instances of {@link AttributeImpl}s.
  */
 public abstract class AttributeFactory {
-
+  
   /**
    * Returns an {@link AttributeImpl} for the supplied {@link Attribute} interface class.
    */
@@ -51,7 +51,7 @@ public abstract class AttributeFactory {
    * class name of the supplied {@link Attribute} interface class by appending <code>Impl</code> to it.
    */
   public static final AttributeFactory DEFAULT_ATTRIBUTE_FACTORY = new DefaultAttributeFactory();
-    
+  
   private static final class DefaultAttributeFactory extends AttributeFactory {
     private final ClassValue<MethodHandle> constructors = new ClassValue<MethodHandle>() {
       @Override
@@ -59,12 +59,11 @@ public abstract class AttributeFactory {
         return findAttributeImplCtor(findImplClass(attClass.asSubclass(Attribute.class)));
       }
     };
-    
+
     DefaultAttributeFactory() {}
-    
+  
     @Override
-    public AttributeImpl createAttributeInstance(
-        Class<? extends Attribute> attClass) {
+    public AttributeImpl createAttributeInstance(Class<? extends Attribute> attClass) {
       try {
         return (AttributeImpl) constructors.get(attClass).invokeExact();
       } catch (Throwable t) {
@@ -82,12 +81,77 @@ public abstract class AttributeFactory {
     }
   }
   
+  /** <b>Expert</b>: AttributeFactory returning an instance of the given {@code clazz} for the
+   * attributes it implements. For all other attributes it calls the given delegate factory
+   * as fallback. This class can be used to prefer a specific {@code AttributeImpl} which
+   * combines multiple attributes over separate classes.
+   * @lucene.internal
+   */
+  public abstract static class StaticImplementationAttributeFactory<A extends AttributeImpl> extends AttributeFactory {
+    private final AttributeFactory delegate;
+    private final Class<A> clazz;
+    
+    /** <b>Expert</b>: Creates an AttributeFactory returning {@code clazz} as instance for the
+     * attributes it implements and for all other attributes calls the given delegate factory. */
+    public StaticImplementationAttributeFactory(AttributeFactory delegate, Class<A> clazz) {
+      this.delegate = delegate;
+      this.clazz = clazz;
+    }
+    
+    @Override
+    public final AttributeImpl createAttributeInstance(Class<? extends Attribute> attClass) {
+      return attClass.isAssignableFrom(clazz) ? createInstance() : delegate.createAttributeInstance(attClass);
+    }
+    
+    /** Creates an instance of {@code A}. */
+    protected abstract A createInstance();
+    
+    @Override
+    public boolean equals(Object other) {
+      if (this == other)
+        return true;
+      if (other == null || other.getClass() != this.getClass())
+        return false;
+      @SuppressWarnings("rawtypes")
+      final StaticImplementationAttributeFactory af = (StaticImplementationAttributeFactory) other;
+      return this.delegate.equals(af.delegate) && this.clazz == af.clazz;
+    }
+    
+    @Override
+    public int hashCode() {
+      return 31 * delegate.hashCode() + clazz.hashCode();
+    }
+  }
+  
+  /** Returns an AttributeFactory returning an instance of the given {@code clazz} for the
+   * attributes it implements. The given {@code clazz} must have a public no-arg constructor.
+   * For all other attributes it calls the given delegate factory as fallback.
+   * This method can be used to prefer a specific {@code AttributeImpl} which combines
+   * multiple attributes over separate classes.
+   * <p>Please save instances created by this method in a static final field, because
+   * on each call, this does reflection for creating a {@link MethodHandle}.
+   */
+  public static <A extends AttributeImpl> AttributeFactory getStaticImplementation(AttributeFactory delegate, Class<A> clazz) {
+    final MethodHandle constr = findAttributeImplCtor(clazz);
+    return new StaticImplementationAttributeFactory<A>(delegate, clazz) {
+      @Override
+      protected A createInstance() {
+        try {
+          return (A) constr.invokeExact();
+        } catch (Throwable t) {
+          rethrow(t);
+          throw new AssertionError();
+        }
+      }
+    };
+  }
+  
   // Hack to rethrow unknown Exceptions from {@link MethodHandle#invoke}:
   // TODO: remove the impl in test-framework, this one is more elegant :-)
   static void rethrow(Throwable t) {
     AttributeFactory.<Error>rethrow0(t);
   }
-
+  
   @SuppressWarnings("unchecked")
   private static <T extends Throwable> void rethrow0(Throwable t) throws T {
     throw (T) t;
